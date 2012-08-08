0782660f2f3675ca4807935d50d22b048fb029b4
Larry Botha <larrywp@themousepotatowebsite.co.za>
2012-08-08 17:05:45 +0200

    add jqueryui autocomplete

--- js/main.js
+++ js/main.js
@@ -62,5 +62,35 @@ $().ready(function() {
 		// Enable tab indents on lists
 		init_instance_callback: fixTinyMCETabIssue
 	});
-// *** TinyMCE ***
+	// *** TinyMCE END ***
+
+	// jQueryUI Autocomplete
+	var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+
+		$( "#tags" ).autocomplete({
+			source: availableTags
+		});
 });
