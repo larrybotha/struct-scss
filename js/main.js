a1e37d77a2d3739545f6ab0392990850822b83d8
Larry Botha <larrywp@themousepotatowebsite.co.za>
2012-11-10 10:36:01 +0200

    remove unused script, update main.js

--- js/main.js
+++ js/main.js
@@ -210,6 +210,136 @@ jQuery(function($) {
 
 
 	//	*****************************************************************************
+	//	Accordion-like Collapse
+	//	*****************************************************************************
+	function UICollapse() {
+		var Collapse = function(element, options) {
+			this.$element = $(element);
+			this.options = $.extend({}, $.fn.collapse.defaults, options);
+
+			if (this.options.parent) {
+				this.$parent = $(this.options.parent);
+			}
+
+			this.options.toggle && this.toggle();
+		};
+
+		Collapse.prototype = {
+
+			constructor: Collapse,
+			dimension: function() {
+				var hasWidth = this.$element.hasClass('width');
+				return hasWidth ? 'width' : 'height';
+			},
+
+			show: function() {
+				var dimension,
+						scroll,
+						actives,
+						hasData;
+
+				if (this.transitioning) return;
+
+				dimension = this.dimension();
+				scroll = $.camelCase(['scroll', dimension].join('-'));
+				actives = this.$parent && this.$parent.find('> .accordion-group > .in');
+
+				if (actives && actives.length) {
+					hasData = actives.data('collapse');
+					if (hasData && hasData.transitioning) return;
+					actives.collapse('hide');
+					hasData || actives.data('collapse', null);
+				}
+
+				this.$element[dimension](0);
+				this.transition('addClass', $.Event('show'), 'shown');
+				$.support.transition && this.$element[dimension](this.$element[0][scroll]);
+			},
+
+			hide: function() {
+				var dimension;
+				if (this.transitioning) return;
+				dimension = this.dimension();
+				this.reset(this.$element[dimension]());
+				this.transition('removeClass', $.Event('hide'), 'hidden');
+				this.$element[dimension](0);
+			},
+
+			reset: function(size) {
+				var dimension = this.dimension();
+
+				this.$element
+					.removeClass('collapse')
+					[dimension](size || 'auto')
+					[0].offsetWidth;
+
+				this.$element[size !== null ? 'addClass' : 'removeClass']('collapse');
+
+				return this;
+			},
+
+			transition: function(method, startEvent, completeEvent) {
+				var that = this,
+						complete = function() {
+							if (startEvent.type == 'show') that.reset();
+							that.transitioning = 0;
+							that.$element.trigger(completeEvent);
+						};
+
+				this.$element.trigger(startEvent);
+
+				if (startEvent.isDefaultPrevented()) return;
+
+				this.transitioning = 1;
+
+				this.$element[method]('collapse-in');
+
+				$.support.transition && this.$element.hasClass('collapse') ?
+					this.$element.one($.support.transition.end, complete) :
+					complete();
+			},
+
+			toggle: function() {
+				this[this.$element.hasClass('collapse-in') ? 'hide' : 'show']();
+			}
+
+		};
+
+
+		/* COLLAPSIBLE PLUGIN DEFINITION
+		* ============================== */
+
+		$.fn.collapse = function(option) {
+			return this.each(function() {
+				var $this = $(this),
+						data = $this.data('collapse'),
+						options = typeof option == 'object' && option;
+				if (!data) $this.data('collapse', (data = new Collapse(this, options)));
+				if (typeof option == 'string') data[option]();
+			});
+		};
+
+		$.fn.collapse.defaults = { toggle: true};
+
+		$.fn.collapse.Constructor = Collapse;
+
+
+		/* COLLAPSIBLE DATA-API
+		* ==================== */
+
+		$(document).on('click.collapse.data-api', '[data-toggle=collapse]', function(e) {
+			var $this = $(this), href,
+					target = $this.attr('data-target') ||
+						e.preventDefault() ||
+						(href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, ''), //strip for ie7
+					option = $(target).data('collapse') ? 'toggle' : $this.data();
+			$this[$(target).hasClass('collapse-in') ? 'addClass' : 'removeClass']('collapsed');
+			$(target).collapse(option);
+		});
+	}
+
+
+	//	*****************************************************************************
 	//	Show Window Width
 	//	*****************************************************************************
 
@@ -272,5 +402,13 @@ jQuery(function($) {
 	});
 
 	$('#powerTip, .alert').on('click', '.close', function(e) { $(this).parent().fadeOut(); });
+
+
+	// READY
+	// Remove .no-js
+	$('.no-js').removeClass('no-js');
+
+	// Collapse
+	UICollapse();
 
 });
