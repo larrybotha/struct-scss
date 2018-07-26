3f8a087c39f1882852a62640970ca1c45909ef7e
Larry Botha <larry@fixate.it>
2018-07-26 17:39:35 +0200

    prettier

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,18 +6,14 @@
 // Simply add a new task there, and it will be automatically available as a gulp
 // task for your project
 
-const gulp         = require("gulp");
-const requireDir   = require("require-dir");
-const browserSync  = require("browser-sync").create();
+const gulp = require('gulp');
+const requireDir = require('require-dir');
+const browserSync = require('browser-sync').create();
 
 global.isWatching = false;
 global.browserSync = browserSync;
 
-requireDir('./gulp/tasks', { recurse: false });
-
-
-
-
+requireDir('./gulp/tasks', {recurse: false});
 
 /*------------------------------------*\
 #     $TASKS
